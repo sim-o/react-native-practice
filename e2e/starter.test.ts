df726745ff4b184247f959389014768a28964733
Simon Kerle <skerle@vmware.com>
2024-02-22 10:22:25 +1100

    detox working with android

--- e2e/starter.test.ts
+++ e2e/starter.test.ts
@@ -10,8 +10,8 @@ describe('Notes', () => {
   });
 
   it('can add a note', async () => {
-    await expect(element(by.text('Add Note'))).toBeVisible();
-    await element(by.text('Add Note')).tap();
+    await expect(element(by.text(button('Add Note')))).toBeVisible();
+    await element(by.text(button('Add Note'))).tap();
     await element(by.text('enter note...')).tap();
 
     await element(by.text('enter note...')).replaceText('this is my note');
@@ -20,3 +20,9 @@ describe('Notes', () => {
     await expect(element(by.text('this is my note'))).toExist();
   });
 });
+
+function button(label: string): string {
+  const recased = device.getPlatform() === 'android' ? label.toLocaleUpperCase() : label;
+  console.log('label', recased)
+  return recased;
+}
