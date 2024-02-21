1887041bdcdcd6242a1b81155be5fd7f4f9b5f6c
Simon Kerle <skerle@vmware.com>
2024-02-21 11:37:10 +1100

    edit and update notes

--- Notes.tsx
+++ Notes.tsx
@@ -1,14 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import {
+  Button,
   FlatList,
-  View,
-  Text,
-  StyleSheet,
   SafeAreaView,
+  StyleSheet,
+  Text,
   TextInput,
+  View,
 } from "react-native";
 import { slice as notesSlice } from "./store/notes";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 const styles = StyleSheet.create({
   title: { fontWeight: "bold", fontSize: 24, textDecorationLine: "underline" },
@@ -16,24 +17,53 @@ const styles = StyleSheet.create({
   noteText: { fontSize: 18 },
 });
 
-function Note({ note }: { note: string }) {
+function Note({
+  note,
+  edit,
+  onTap,
+  onChange,
+}: {
+  note: string;
+  edit: boolean;
+  onTap: () => void;
+  onChange: (value: string) => void;
+}) {
   return (
-    <View style={styles.note}>
-      <Text style={styles.noteText}>{note}</Text>
+    <View style={styles.note} onTouchEnd={onTap}>
+      {!edit ? (
+        <Text style={styles.noteText}>{note}</Text>
+      ) : (
+        <TextInput value={note} onChangeText={(v) => onChange(v)} />
+      )}
     </View>
   );
 }
 
 export function Notes() {
-  const notes: string[] = useSelector(notesSlice.selectors.selectNotes);
+  const dispatch = useDispatch();
+  const notes = useSelector(notesSlice.selectors.selectNotes);
+  const [editing, setEditing] = useState(-1);
+
   return (
     <View>
       <SafeAreaView>
         <Text style={styles.title}>This is the top</Text>
         <FlatList
           data={notes}
-          renderItem={(note) => <Note note={note.item} />}
+          renderItem={(note) => (
+            <Note
+              note={note.item.value}
+              edit={note.item.id === editing}
+              onTap={() => {
+                setEditing(note.item.id);
+              }}
+              onChange={(value) => {
+                dispatch(notesSlice.actions.updateNote({ id: note.item.id, value }))
+              }}
+            />
+          )}
         />
+        <Button title="Add Note" onPress={() => dispatch(notesSlice.actions.newNote('enter note...'))}/>
       </SafeAreaView>
     </View>
   );
--- store/notes.ts
+++ store/notes.ts
@@ -1,18 +1,33 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 
-type State = { notes: string[] };
+export type Note = { id: number; value: string };
+export type State = { nextId: number; notes: Note[] };
 
-const initialState: State = { notes: ["first note", "second note"] };
+const initialState: State = {
+  nextId: 3,
+  notes: [
+    { id: 1, value: "first note" },
+    { id: 2, value: "second note" },
+  ],
+};
 
 export const slice = createSlice({
   name: "notes",
   initialState,
   selectors: {
-    selectNotes: (state): string[] => state.notes,
+    selectNotes: (state): Note[] => state.notes,
   },
   reducers: {
-    newNote: (state, { payload }: { payload: string }) => {
-      state.notes.push(payload);
+    newNote: (state, { payload }: PayloadAction<string>) => {
+      const nextId = state.nextId++;
+      state.notes.push({ id: nextId, value: payload });
     },
+    updateNote: (state, { payload }: PayloadAction<Note>) => {
+      const note = state.notes.find(({ id }) => id === payload.id);
+      if (note) {
+        note.value = payload.value;
+      }
+    }
   },
 });
+
